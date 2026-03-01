64f83bf4656ba6d7f5a4265618fdb043f21ec03f
EarthOwnerHell <shekaandalex@gmail.com>
2026-03-01 21:57:21 +0300

    feat, fixes: добавлена новая страница (SportPage), фикс пары багов

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import MainPage from "./pages/MainPage";
 import PromotionPage from "./pages/PromotionPage";
 import PaymentPage from "./pages/PaymentPage";
 import RentPage from "./pages/RentPage";
+import SportPage from "./pages/SportPage";
 import { Navigate, Route, Routes } from "react-router-dom";
 import AuthPage from "./pages/AuthPage";
 import PlaceholderPage from "./pages/PlaceholderPage";
@@ -15,7 +16,7 @@ function App() {
       <Route path="/schedule" element={<Navigate to="/" replace />} />
       <Route path="/cinemas" element={<PlaceholderPage title="Кинотеатры" />} />
       <Route path="/events" element={<PlaceholderPage title="События" />} />
-      <Route path="/sport" element={<PlaceholderPage title="Спорт" />} />
+      <Route path="/sport" element={<SportPage />} />
       <Route path="/rent" element={<RentPage />} />
       <Route path="/offers" element={<PromotionPage />} />
       <Route path="/payment" element={<PaymentPage />} />
